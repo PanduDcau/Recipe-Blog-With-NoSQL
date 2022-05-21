3b6d32f988bb196ad730ab3df638256a9ca2095b
Pandula1234 <52339029+Pandula1234@users.noreply.github.com>
2022-05-21 21:36:38 +0530

    routes adding

---
+++ server/routes/recipeRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const recipeController = require('../controllers/recipeController');
+
+/**
+ * App Routes 
+*/
+router.get('/', recipeController.homepage);
+router.get('/recipe/:id', recipeController.exploreRecipe );
+router.get('/categories', recipeController.exploreCategories);
+router.get('/categories/:id', recipeController.exploreCategoriesById);
+router.post('/search', recipeController.searchRecipe);
+router.get('/explore-latest', recipeController.exploreLatest);
+router.get('/explore-random', recipeController.exploreRandom);
+router.get('/submit-recipe', recipeController.submitRecipe);
+router.post('/submit-recipe', recipeController.submitRecipeOnPost);
+router.get('/contact', recipeController.contact);
+router.get('/explore', recipeController.explore);
+
+ 
+module.exports = router;
