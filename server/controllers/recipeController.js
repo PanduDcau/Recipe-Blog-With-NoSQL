db32bf005e3f2fcbdaa2fdc549c0c28321ebad47
Pandula1234 <52339029+Pandula1234@users.noreply.github.com>
2022-05-21 21:38:33 +0530

    Recipe

---
+++ server/controllers/recipeController.js
@@ -0,0 +1,299 @@
+require('../models/database');
+const Category = require('../models/Category');
+const Recipe = require('../models/Recipe');
+
+/**
+ * GET /
+ * Homepage 
+*/
+exports.homepage = async(req, res) => {
+  try {
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+    const latest = await Recipe.find({}).sort({_id: -1}).limit(limitNumber);
+    const srilankan = await Recipe.find({ 'category': 'SriLankan' }).limit(limitNumber);
+    const american = await Recipe.find({ 'category': 'American' }).limit(limitNumber);
+    const chinese = await Recipe.find({ 'category': 'Chinese' }).limit(limitNumber);
+
+    const food = { latest, srilankan, american, chinese };
+
+    res.render('index', { title: 'Cooking Blog - Home', categories, food } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+}
+
+/**
+ * GET /categories
+ * Categories 
+*/
+exports.exploreCategories = async(req, res) => {
+  try {
+    const limitNumber = 20;
+    const categories = await Category.find({}).limit(limitNumber);
+    res.render('categories', { title: 'Cooking Blog - Categoreis', categories } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+
+/**
+ * GET /categories/:id
+ * Categories By Id
+*/
+exports.exploreCategoriesById = async(req, res) => { 
+  try {
+    let categoryId = req.params.id;
+    const limitNumber = 20;
+    const categoryById = await Recipe.find({ 'category': categoryId }).limit(limitNumber);
+    res.render('categories', { title: 'Cooking Blog - Categoreis', categoryById } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+ 
+/**
+ * GET /recipe/:id
+ * Recipe 
+*/
+exports.exploreRecipe = async(req, res) => {
+  try {
+    let recipeId = req.params.id;
+    const recipe = await Recipe.findById(recipeId);
+    res.render('recipe', { title: 'Cooking Blog - Recipe', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+
+/**
+ * POST /search
+ * Search 
+*/
+exports.searchRecipe = async(req, res) => {
+  try {
+    let searchTerm = req.body.searchTerm;
+    let recipe = await Recipe.find( { $text: { $search: searchTerm, $diacriticSensitive: true } });
+    res.render('search', { title: 'Cooking Blog - Search', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+  
+}
+
+/**
+ * GET /explore-latest
+ * Explplore Latest 
+*/
+exports.exploreLatest = async(req, res) => {
+  try {
+    const limitNumber = 20;
+    const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+    res.render('explore-latest', { title: 'Cooking Blog - Explore Latest', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+
+
+/**
+ * GET /explore-random
+ * Explore Random as JSON
+*/
+exports.exploreRandom = async(req, res) => {
+  try {
+    let count = await Recipe.find().countDocuments();
+    let random = Math.floor(Math.random() * count);
+    let recipe = await Recipe.findOne().skip(random).exec();
+    res.render('explore-random', { title: 'Cooking Blog - Explore Latest', recipe } );
+  } catch (error) {
+    res.satus(500).send({message: error.message || "Error Occured" });
+  }
+} 
+
+
+/**
+ * GET /submit-recipe
+ * Submit Recipe
+*/
+exports.submitRecipe = async(req, res) => {
+  const infoErrorsObj = req.flash('infoErrors');
+  const infoSubmitObj = req.flash('infoSubmit');
+  res.render('submit-recipe', { title: 'Cooking Blog - Submit Recipe', infoErrorsObj, infoSubmitObj  } );
+}
+
+/**
+ * POST /submit-recipe
+ * Submit Recipe
+*/
+exports.submitRecipeOnPost = async(req, res) => {
+  try {
+
+    let imageUploadFile;
+    let uploadPath;
+    let newImageName;
+
+    if(!req.files || Object.keys(req.files).length === 0){
+      console.log('No Files where uploaded.');
+    } else {
+
+      imageUploadFile = req.files.image;
+      newImageName = Date.now() + imageUploadFile.name;
+
+      uploadPath = require('path').resolve('./') + '/public/uploads/' + newImageName;
+
+      imageUploadFile.mv(uploadPath, function(err){
+        if(err) return res.satus(500).send(err);
+      })
+
+    }
+
+    const newRecipe = new Recipe({
+      name: req.body.name,
+      description: req.body.description,
+      email: req.body.email,
+      ingredients: req.body.ingredients,
+      category: req.body.category,
+      image: newImageName
+    });
+    
+    await newRecipe.save();
+
+    req.flash('infoSubmit', 'Recipe has been added.')
+    res.redirect('/submit-recipe');
+  } catch (error) {
+    // res.json(error);
+    req.flash('infoErrors', error);
+    res.redirect('/submit-recipe');
+  }
+}
+
+/**
+ * GET /contact
+*/
+
+
+exports.contact = async(req, res) => {
+  const infoErrorsObj = req.flash('infoErrors');
+  const infoSubmitObj = req.flash('infoSubmit');
+  res.render('contact', { title: 'Cooking Blog - About Us' ,infoErrorsObj, infoSubmitObj } );
+}
+
+
+/**
+ * GET /aboutus
+*/
+
+
+exports.explore = async(req, res) => {
+  const infoErrorsObj = req.flash('infoErrors');
+  const infoSubmitObj = req.flash('infoSubmit');
+  res.render('explore', { title: 'Cooking Blog - For More Cuisine Secrets',infoErrorsObj, infoSubmitObj  } );
+}
+
+
+
+
+
+// Delete Recipe
+// async function deleteRecipe(){
+//   try {
+//     await Recipe.deleteOne({ name: 'New Recipe From Form' });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// deleteRecipe();
+
+
+// Update Recipe
+// async function updateRecipe(){
+//   try {
+//     const res = await Recipe.updateOne({ name: 'New Recipe' }, { name: 'New Recipe Updated' });
+//     res.n; // Number of documents matched
+//     res.nModified; // Number of documents modified
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// updateRecipe();
+
+
+/**
+ * Use this Data Segement to store Categories in MongoDB
+*/
+
+// async function insertDymmyCategoryData(){
+//   try {
+//     await Category.insertMany([
+//       {
+//         "name": "SriLankan",
+//         "image": "thai-food.jpg"
+//       },
+//       {
+//         "name": "American",
+//         "image": "american-food.jpg"
+//       }, 
+//       {
+//         "name": "Chinese",
+//         "image": "chinese-food.jpg"
+//       },
+//       {
+//         "name": "Mexican",
+//         "image": "mexican-food.jpg"
+//       }, 
+//       {
+//         "name": "Indian",
+//         "image": "indian-food.jpg"
+//       },
+//       {
+//         "name": "Spanish",
+//         "image": "spanish-food.jpg"
+//       }
+//     ]);
+//   } catch (error) {
+//     console.log('err', + error)
+//   }
+// }
+
+// insertDymmyCategoryData();
+
+
+// async function insertDymmyRecipeData(){
+//   try {
+//     await Recipe.insertMany([
+//       { 
+//         "name": "Recipe Name Goes Here",
+//         "description": `Recipe Description Goes Here`,
+//         "email": "recipeemail@raddy.co.uk",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "1 level teaspoon hot smoked paprika",
+//         ],
+//         "category": "American", 
+//         "image": "chocolate-banoffe-whoopie-pies.jpg"
+//       },
+//       { 
+//         "name": "Recipe Name Goes Here",
+//         "description": `Recipe Description Goes Here`,
+//         "email": "recipeemail@raddy.co.uk",
+//         "ingredients": [
+//           "1 level teaspoon baking powder",
+//           "1 level teaspoon cayenne pepper",
+//           "1 level teaspoon hot smoked paprika",
+//         ],
+//         "category": "American", 
+//         "image": "southern-friend-chicken.jpg"
+//       },
+//     ]);
+//   } catch (error) {
+//     console.log('err', + error)
+//   }
+// }
+
+// insertDymmyRecipeData();
+
